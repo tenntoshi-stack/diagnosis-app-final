1d15993079616619d20017744fcef5caa351210a
toshisada endo <tenntoshi@gmail.com>
2026-01-06 10:01:49 +0900

    Fix: Add Router and defined routes for diagnosis ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DiagnosisApp from './DiagnosisApp';
 
 function App() {
   return (
-    <div className="App">
-      <DiagnosisApp />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* 通常のアクセス用 */}
+          <Route path="/" element={<DiagnosisApp />} />
+          {/* 管理画面からのID付きアクセス用 */}
+          <Route path="/diagnoses/:id" element={<DiagnosisApp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
